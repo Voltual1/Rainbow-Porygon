180535ad6e4969e6cd3ead446a155ab62c4ba7e7
Voltual1 <18206039173@163.com>
2026-07-19 17:37:42 +0800

    修复include/gba/defines.h的宏定义&引入多平台的include/cgb_audio.h头文件

--- include/gba/defines.h
+++ include/gba/defines.h
@@ -3,20 +3,32 @@
 
 #include <stddef.h>
 
-#define TRUE   1
-#define FALSE  0
+#define TRUE  1
+#define FALSE 0
 
-#define IWRAM_DATA __attribute__((section(".bss")))
-#define EWRAM_DATA __attribute__((section(".sbss")))
-#define IWRAM_INIT __attribute__((section(".iwram")))
-#define EWRAM_INIT __attribute__((section(".ewram")))
-#define COMMON_DATA __attribute__((section("common_data")))
+// 内存段属性与编译器优化 (融合自后者，支持现代 GCC)
+#ifdef PORTABLE
+    // 原生移植平台不需要 GBA 的特殊内存段
+    #define IWRAM_DATA
+    #define EWRAM_DATA
+    #define IWRAM_INIT
+    #define EWRAM_INIT
+    #define COMMON_DATA
+    #define ARM_FUNC 
+#else
+    // GBA 硬件平台专属内存段
+    #define IWRAM_DATA __attribute__((section(".bss")))
+    #define EWRAM_DATA __attribute__((section(".sbss")))
+    #define IWRAM_INIT __attribute__((section(".iwram")))
+    #define EWRAM_INIT __attribute__((section(".ewram")))
+    #define COMMON_DATA __attribute__((section("common_data")))
+    #define ARM_FUNC __attribute__((target("arm")))
+#endif
+
 #define UNUSED __attribute__((unused))
 #define USED __attribute__((used))
 #define KEEP_SECTION __attribute__((section(".text.consts")))
 #define DEPRECATED(msg) __attribute__((deprecated(msg)))
-
-#define ARM_FUNC __attribute__((target("arm")))
 
 #if MODERN
 #define NOINLINE __attribute__((noinline))
@@ -30,27 +42,44 @@
 #define ALWAYS_INLINE inline __attribute__((always_inline))
 #define NONNULL __attribute__((__nonnull__))
 
-#define SOUND_INFO_PTR (*(struct SoundInfo **)0x3007FF0)
-#define INTR_CHECK     (*(u16 *)0x3007FF8)
-#define INTR_VECTOR    (*(void **)0x3007FFC)
-
-#define ROM_START 0x8000000
-#define ROM_END 0xA000000
-
-#define EWRAM_START 0x02000000
-#define EWRAM_END   (EWRAM_START + 0x40000)
-#define IWRAM_START 0x03000000
-#define IWRAM_END   (IWRAM_START + 0x8000)
-
-#define PLTT          0x5000000
-#define BG_PLTT       PLTT
+// 硬件地址与指针映射
 #define BG_PLTT_SIZE  0x200
-#define OBJ_PLTT      (PLTT + BG_PLTT_SIZE)
 #define OBJ_PLTT_SIZE 0x200
 #define PLTT_SIZE     (BG_PLTT_SIZE + OBJ_PLTT_SIZE)
+#define VRAM_SIZE     0x18000
+#define OAM_SIZE      0x400
 
-#define VRAM      0x6000000
-#define VRAM_SIZE 0x18000
+#ifndef PORTABLE
+    // GBA 实机环境：直接映射到物理 MMIO 地址
+    #define SOUND_INFO_PTR (*(struct SoundInfo **)0x3007FF0)
+    #define INTR_CHECK     (*(u16 *)0x3007FF8)
+    #define INTR_VECTOR    (*(void **)0x3007FFC)
+
+    #define ROM_START   0x8000000
+    #define ROM_END     0xA000000
+    #define EWRAM_START 0x02000000
+    #define EWRAM_END   (EWRAM_START + 0x40000)
+    #define IWRAM_START 0x03000000
+    #define IWRAM_END   (IWRAM_START + 0x8000)
+
+    #define PLTT        0x5000000
+    #define VRAM        0x6000000
+    #define OAM         0x7000000
+#else
+    // 原生移植环境：声明为外部全局数组，由 src/platform/ 内部提供内存实体
+    extern struct SoundInfo * SOUND_INFO_PTR;
+    extern unsigned short INTR_CHECK;
+    extern void * INTR_VECTOR;
+
+    extern unsigned char PLTT[PLTT_SIZE] ALIGNED(4);
+    extern unsigned char VRAM_[VRAM_SIZE] ALIGNED(4);
+    #define VRAM ((uintptr_t)VRAM_)
+    extern unsigned char OAM[OAM_SIZE] ALIGNED(4);
+#endif
+
+// 显存与显示相关宏定义
+#define BG_PLTT           PLTT
+#define OBJ_PLTT          (PLTT + BG_PLTT_SIZE)
 
 #define BG_VRAM           VRAM
 #define BG_VRAM_SIZE      0x10000
@@ -65,15 +94,12 @@
 #define NUM_BACKGROUNDS 4
 
 // text-mode BG
-#define OBJ_VRAM0      (VRAM + 0x10000)
-#define OBJ_VRAM0_SIZE 0x8000
+#define OBJ_VRAM0       (VRAM + 0x10000)
+#define OBJ_VRAM0_SIZE  0x8000
 
 // bitmap-mode BG
-#define OBJ_VRAM1      (VRAM + 0x14000)
-#define OBJ_VRAM1_SIZE 0x4000
-
-#define OAM      0x7000000
-#define OAM_SIZE 0x400
+#define OBJ_VRAM1       (VRAM + 0x14000)
+#define OBJ_VRAM1_SIZE  0x4000
 
 #define ROM_HEADER_SIZE   0xC0
 
