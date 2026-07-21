b0f07e02f90216f2c8869ea2cfe6b808d1f7f08a
Voltual1 <18206039173@163.com>
2026-07-21 22:17:26 +0800

    Refactor AsmFile class constructor and methods

--- tools/preproc/asm_file.cpp
+++ tools/preproc/asm_file.cpp
@@ -33,16 +33,394 @@ AsmFile::AsmFile(std::string filename, bool isStdin, bool doEnum) : m_filename(f
 {
     m_buffer = ReadFileToBuffer(filename.c_str(), isStdin, &m_size);
     m_doEnum = doEnum;
-
     m_pos = 0;
     m_lineNum = 1;
     m_lineStart = 0;
-
     RemoveComments();
 }
 
 AsmFile::AsmFile(AsmFile&& other) : m_filename(std::move(other.m_filename))
 {
+    m_buffer = other.m_buffer;
+    m_doEnum = other.m_doEnum;
+    m_pos = other.m_pos;
+    m_size = other.m_size;
+    m_lineNum = other.m_lineNum;
+    m_lineStart = other.m_lineStart;
+    other.m_buffer = nullptr;
+}
+
+AsmFile::~AsmFile()
+{
+    if (m_size > 0 && m_buffer != nullptr) delete[] m_buffer;
+}
+
+void AsmFile::RemoveComments()
+{
+    long pos = 0;
+    char stringChar = 0;
+    for (;;)
+    {
+        if (m_buffer[pos] == 0) return;
+        if (stringChar != 0) {
+            if (m_buffer[pos] == '\\' && m_buffer[pos + 1] == stringChar) pos += 2;
+            else { if (m_buffer[pos] == stringChar) stringChar = 0; pos++; }
+        } else if (m_buffer[pos] == '@' && (pos == 0 || m_buffer[pos - 1] != '\\')) {
+            while (m_buffer[pos] != '\n' && m_buffer[pos] != 0) m_buffer[pos++] = ' ';
+        } else if (m_buffer[pos] == '/' && m_buffer[pos + 1] == '*') {
+            m_buffer[pos++] = ' '; m_buffer[pos++] = ' ';
+            for (;;) {
+                if (m_buffer[pos] == 0) return;
+                if (m_buffer[pos] == '*' && m_buffer[pos + 1] == '/') {
+                    m_buffer[pos++] = ' '; m_buffer[pos++] = ' '; break;
+                } else { if (m_buffer[pos] != '\n') m_buffer[pos] = ' '; pos++; }
+            }
+        } else {
+            if (m_buffer[pos] == '"' || m_buffer[pos] == '\'') stringChar = m_buffer[pos];
+            pos++;
+        }
+    }
+}
+
+bool AsmFile::CheckForDirective(std::string name)
+{
+    long i;
+    long length = static_cast<long>(name.length());
+    for (i = 0; i < length && m_pos + i < m_size; i++)
+        if (name[i] != m_buffer[m_pos + i]) return false;
+    if (i < length) return false;
+    m_pos += length;
+    return true;
+}
+
+Directive AsmFile::GetDirective()
+{
+    SkipWhitespace();
+    if (CheckForDirective(".include")) return Directive::Include;
+    else if (CheckForDirective(".string")) return Directive::String;
+    else if (CheckForDirective(".braille")) return Directive::Braille;
+    else if (CheckForDirective("enum")) return Directive::Enum;
+    else if (CheckForDirective(".macro")) return Directive::Macro;
+    else return Directive::Unknown;
+}
+
+Label AsmFile::GetLabel()
+{
+    long start = m_pos;
+    long pos = m_pos;
+    if (IsIdentifierStartingChar(m_buffer[pos])) {
+        pos++;
+        while (IsIdentifierChar(m_buffer[pos])) pos++;
+    }
+    if (m_buffer[pos] == ':') {
+        std::string symbol(&m_buffer[start], pos - start);
+        if (m_buffer[pos + 1] == ':') {
+            m_pos = pos + 2; ExpectEmptyRestOfLine();
+            return Label(symbol, Label::global);
+        } else {
+            m_pos = pos + 1; return Label(symbol, Label::local);
+        }
+    }
+    return Label("", Label::none);
+}
+
+std::string AsmFile::PeekSection()
+{
+    long oldPos = m_pos;
+    std::string section;
+    SkipWhitespace();
+    if (CheckForDirective(".bss")) section = ".bss";
+    else if (CheckForDirective(".data")) section = ".data";
+    else if (CheckForDirective(".rodata")) section = ".rodata";
+    else if (CheckForDirective(".text")) section = ".text";
+    else if (CheckForDirective(".section")) { SkipWhitespace(); section = ReadIdentifier(); }
+    m_pos = oldPos;
+    return section;
+}
+
+void AsmFile::SkipWhitespace()
+{
+    while (m_buffer[m_pos] == '\t' || m_buffer[m_pos] == ' ') m_pos++;
+}
+
+std::string AsmFile::ReadPath()
+{
+    SkipWhitespace();
+    if (m_buffer[m_pos] != '"') RaiseError("expected file path");
+    m_pos++;
+    int length = 0;
+    long startPos = m_pos;
+    while (m_buffer[m_pos] != '"') {
+        unsigned char c = m_buffer[m_pos++];
+        if (c == 0) RaiseError("unexpected EOF in include string");
+        if (!IsAsciiPrintable(c)) RaiseError("unexpected character '\\x%02X' in include string", c);
+        length++;
+        if (length > kMaxPath) RaiseError("path is too long");
+    }
+    m_pos++;
+    ExpectEmptyRestOfLine();
+    return std::string(&m_buffer[startPos], length);
+}
+
+int AsmFile::ReadString(unsigned char* s)
+{
+    SkipWhitespace();
+    int length;
+    StringParser stringParser(m_buffer, m_size);
+    try { m_pos += stringParser.ParseString(m_pos, s, length); }
+    catch (std::runtime_error& e) { RaiseError(e.what()); }
+    SkipWhitespace();
+    if (ConsumeComma()) {
+        SkipWhitespace();
+        int padLength = ReadPadLength();
+        while (length < padLength) s[length++] = CHAR_SPACE;
+    }
+    ExpectEmptyRestOfLine();
+    return length;
+}
+
+void AsmFile::VerifyStringLength(int length)
+{
+    if (length == kMaxStringLength) RaiseError("mapped string longer than %d bytes", kMaxStringLength);
+}
+
+int AsmFile::ReadBraille(unsigned char* s)
+{
+    static std::map<char, unsigned char> encoding = {
+        { 'A', BRAILLE_CHAR_A }, { 'B', BRAILLE_CHAR_B }, { 'C', BRAILLE_CHAR_C }, { 'D', BRAILLE_CHAR_D },
+        { 'E', BRAILLE_CHAR_E }, { 'F', BRAILLE_CHAR_F }, { 'G', BRAILLE_CHAR_G }, { 'H', BRAILLE_CHAR_H },
+        { 'I', BRAILLE_CHAR_I }, { 'J', BRAILLE_CHAR_J }, { 'K', BRAILLE_CHAR_K }, { 'L', BRAILLE_CHAR_L },
+        { 'M', BRAILLE_CHAR_M }, { 'N', BRAILLE_CHAR_N }, { 'O', BRAILLE_CHAR_O }, { 'P', BRAILLE_CHAR_P },
+        { 'Q', BRAILLE_CHAR_Q }, { 'R', BRAILLE_CHAR_R }, { 'S', BRAILLE_CHAR_S }, { 'T', BRAILLE_CHAR_T },
+        { 'U', BRAILLE_CHAR_U }, { 'V', BRAILLE_CHAR_V }, { 'W', BRAILLE_CHAR_W }, { 'X', BRAILLE_CHAR_X },
+        { 'Y', BRAILLE_CHAR_Y }, { 'Z', BRAILLE_CHAR_Z }, { 'a', BRAILLE_CHAR_A }, { 'b', BRAILLE_CHAR_B },
+        { 'c', BRAILLE_CHAR_C }, { 'd', BRAILLE_CHAR_D }, { 'e', BRAILLE_CHAR_E }, { 'f', BRAILLE_CHAR_F },
+        { 'g', BRAILLE_CHAR_G }, { 'h', BRAILLE_CHAR_H }, { 'i', BRAILLE_CHAR_I }, { 'j', BRAILLE_CHAR_J },
+        { 'k', BRAILLE_CHAR_K }, { 'l', BRAILLE_CHAR_L }, { 'm', BRAILLE_CHAR_M }, { 'n', BRAILLE_CHAR_N },
+        { 'o', BRAILLE_CHAR_O }, { 'p', BRAILLE_CHAR_P }, { 'q', BRAILLE_CHAR_Q }, { 'r', BRAILLE_CHAR_R },
+        { 's', BRAILLE_CHAR_S }, { 't', BRAILLE_CHAR_T }, { 'u', BRAILLE_CHAR_U }, { 'v', BRAILLE_CHAR_V },
+        { 'w', BRAILLE_CHAR_W }, { 'x', BRAILLE_CHAR_X }, { 'y', BRAILLE_CHAR_Y }, { 'z', BRAILLE_CHAR_Z },
+        { '0', BRAILLE_CHAR_0 }, { '1', BRAILLE_CHAR_1 }, { '2', BRAILLE_CHAR_2 }, { '3', BRAILLE_CHAR_3 },
+        { '4', BRAILLE_CHAR_4 }, { '5', BRAILLE_CHAR_5 }, { '6', BRAILLE_CHAR_6 }, { '7', BRAILLE_CHAR_7 },
+        { '8', BRAILLE_CHAR_8 }, { '9', BRAILLE_CHAR_9 }, { ' ', BRAILLE_CHAR_SPACE }, { ',', BRAILLE_CHAR_COMMA },
+        { '.', BRAILLE_CHAR_PERIOD }, { '?', BRAILLE_CHAR_QUESTION_MARK }, { '!', BRAILLE_CHAR_EXCL_MARK },
+        { ':', BRAILLE_CHAR_COLON }, { ';', BRAILLE_CHAR_SEMICOLON }, { '-', BRAILLE_CHAR_HYPHEN },
+        { '/', BRAILLE_CHAR_SLASH }, { '(', BRAILLE_CHAR_PAREN }, { ')', BRAILLE_CHAR_PAREN },
+        { '\'', BRAILLE_CHAR_APOSTROPHE }, { '#', BRAILLE_CHAR_NUMBER }, { '$', EOS },
+    };
+    SkipWhitespace();
+    int length = 0;
+    if (m_buffer[m_pos] != '"') RaiseError("expected braille string literal");
+    m_pos++;
+    bool inNumber = false;
+    while (m_buffer[m_pos] != '"') {
+        if (m_buffer[m_pos] == '\\' && m_buffer[m_pos + 1] == 'n') {
+            VerifyStringLength(length); s[length++] = CHAR_NEWLINE; m_pos += 2;
+        } else {
+            char c = m_buffer[m_pos];
+            if (encoding.count(c) == 0) RaiseError("character not valid in braille string");
+            if (!inNumber && c >= '0' && c <= '9') { inNumber = true; VerifyStringLength(length); s[length++] = BRAILLE_CHAR_NUMBER; }
+            else if (inNumber && encoding[c] == BRAILLE_CHAR_SPACE) inNumber = false;
+            VerifyStringLength(length); s[length++] = encoding[c]; m_pos++;
+        }
+    }
+    m_pos++; ExpectEmptyRestOfLine();
+    return length;
+}
+
+bool AsmFile::ConsumeComma()
+{
+    if (m_buffer[m_pos] == ',') { m_pos++; return true; }
+    return false;
+}
+
+static int ConvertDigit(char c, int radix)
+{
+    int digit;
+    if (c >= '0' && c <= '9') digit = c - '0';
+    else if (c >= 'A' && c <= 'F') digit = 10 + c - 'A';
+    else if (c >= 'a' && c <= 'f') digit = 10 + c - 'a';
+    else return -1;
+    return (digit < radix) ? digit : -1;
+}
+
+int AsmFile::ReadPadLength()
+{
+    if (!IsAsciiDigit(m_buffer[m_pos])) RaiseError("expected integer");
+    int radix = 10;
+    if (m_buffer[m_pos] == '0' && m_buffer[m_pos + 1] == 'x') { radix = 16; m_pos += 2; }
+    unsigned n = 0; int digit;
+    while ((digit = ConvertDigit(m_buffer[m_pos], radix)) != -1) {
+        n = n * radix + digit;
+        if (n > kMaxStringLength) RaiseError("pad length greater than maximum length");
+        m_pos++;
+    }
+    return n;
+}
+
+void AsmFile::OutputLine()
+{
+    while (m_buffer[m_pos] != '\n' && m_buffer[m_pos] != 0) m_pos++;
+    if (m_buffer[m_pos] == 0) {
+        if (m_pos >= m_size) { RaiseWarning("file doesn't end with newline"); puts(&m_buffer[m_lineStart]); }
+        else RaiseError("unexpected null character");
+    } else {
+        m_buffer[m_pos] = 0; puts(&m_buffer[m_lineStart]);
+        m_buffer[m_pos] = '\n'; m_pos++; m_lineStart = m_pos; m_lineNum++;
+    }
+}
+
+bool AsmFile::ParseEnum()
+{
+    if (!m_doEnum) return false;
+    long fallbackPosition = m_pos;
+    std::string headerFilename = "unknown";
+    long currentHeaderLine = 0;
+    while (true) {
+        currentHeaderLine += SkipWhitespaceAndEol();
+        std::string identifier = ReadIdentifier();
+        if (identifier == "__attribute__") {
+            if (m_pos + 1 >= m_size || m_buffer[m_pos] != '(' || m_buffer[m_pos + 1] != '(') {
+                m_pos = fallbackPosition - 4; return false;
+            }
+            m_pos += 2; int parens = 2;
+            while (true) {
+                char c = m_buffer[m_pos++];
+                if (c == '\n') currentHeaderLine++;
+                if (c == '(') parens++;
+                else if (c == ')') { parens--; if (parens == 0) break; }
+                else if (parens < 2 || m_pos == m_size) { m_pos = fallbackPosition - 4; return false; }
+            }
+        } else break;
+    }
+    currentHeaderLine += SkipWhitespaceAndEol();
+    std::string enumBase = "0";
+    long enumCounter = 0, symbolCount = 0;
+    if (m_buffer[m_pos] == ':') {
+        m_pos++; std::string underlyingType;
+        do { currentHeaderLine += SkipWhitespaceAndEol(); underlyingType = ReadIdentifier(); } while (!underlyingType.empty());
+    }
+    if (m_buffer[m_pos] != '{') { m_pos = fallbackPosition - 4; return false; }
+
+    currentHeaderLine += FindLastLineNumber(headerFilename);
+    m_pos++;
+    for (;;) {
+        currentHeaderLine += SkipWhitespaceAndEol();
+        std::string currentIdentName = ReadIdentifier();
+        if (!currentIdentName.empty()) {
+            std::printf("# %ld \"%s\"\n", currentHeaderLine, headerFilename.c_str());
+            currentHeaderLine += SkipWhitespaceAndEol();
+            if (m_buffer[m_pos] == '=') {
+                m_pos++; SkipWhitespace(); enumBase.clear();
+                for (;;) {
+                    if (m_pos == m_size) RaiseError("unexpected EOF");
+                    if (m_buffer[m_pos] == ',') break;
+                    if (m_buffer[m_pos] == '\n') { currentHeaderLine++; enumBase.push_back(' '); }
+                    else enumBase.push_back(m_buffer[m_pos]);
+                    m_pos++;
+                }
+                enumCounter = 0;
+            }
+            // 移植模式下：避免输出 .global 符号定义，防止 R_ARM_ABS16 错误。
+            if (std::getenv("PORTABLE") == nullptr) std::printf(".global %s; ", currentIdentName.c_str());
+            std::printf(".equiv %s, (%s) + %ld\n", currentIdentName.c_str(), enumBase.c_str(), enumCounter);
+            enumCounter++; symbolCount++;
+        } else if (symbolCount == 0) {
+            // 放宽限制：解析不出符号则直接返回，不报错。
+            return true;
+        }
+        if (m_buffer[m_pos] == '#') currentHeaderLine = ParseLineSkipInEnum();
+        else if (m_buffer[m_pos] != ',') {
+            currentHeaderLine += SkipWhitespaceAndEol();
+            if (m_buffer[m_pos++] == '}' && m_buffer[m_pos++] == ';') { ExpectEmptyRestOfLine(); break; }
+            else RaiseError("unterminated enum");
+        }
+        m_pos++;
+    }
+    return true;
+}
+
+void AsmFile::ExpectEmptyRestOfLine()
+{
+    SkipWhitespace();
+    if (m_buffer[m_pos] == 0) { if (m_pos >= m_size) RaiseWarning("file doesn't end with newline"); else RaiseError("unexpected null character"); }
+    else if (m_buffer[m_pos] == '\n') { m_pos++; m_lineStart = m_pos; m_lineNum++; }
+    else if (m_buffer[m_pos] == '\r' && m_buffer[m_pos + 1] == '\n') { m_pos += 2; m_lineStart = m_pos; m_lineNum++; }
+    else RaiseError("junk at end of line");
+}
+
+bool AsmFile::IsAtEnd() { return (m_pos >= m_size); }
+void AsmFile::OutputLocation() { std::printf("# %ld \"%s\"\n", m_lineNum, m_filename.c_str()); }
+
+void AsmFile::ReportDiagnostic(const char* type, const char* format, std::va_list args)
+{
+    const int bufferSize = 1024; char buffer[bufferSize];
+    std::vsnprintf(buffer, bufferSize, format, args);
+    std::fprintf(stderr, "%s:%ld: %s: %s\n", m_filename.c_str(), m_lineNum, type, buffer);
+}
+
+void AsmFile::RaiseError(const char* format, ...)
+{
+    std::va_list args; va_start(args, format); ReportDiagnostic("error", format, args); va_end(args);
+    std::exit(1);
+}
+
+void AsmFile::RaiseWarning(const char* format, ...)
+{
+    std::va_list args; va_start(args, format); ReportDiagnostic("warning", format, args); va_end(args);
+}
+
+int AsmFile::SkipWhitespaceAndEol()
+{
+    int newlines = 0;
+    while (m_buffer[m_pos] == '\t' || m_buffer[m_pos] == ' ' || m_buffer[m_pos] == '\r' || m_buffer[m_pos] == '\n') {
+        if (m_buffer[m_pos] == '\n') newlines++;
+        m_pos++;
+    }
+    return newlines;
+}
+
+int AsmFile::ParseLineSkipInEnum(void)
+{
+    m_pos++; while (m_buffer[m_pos] == ' ' || m_buffer[m_pos] == '\t') m_pos++;
+    if (!IsAsciiDigit(m_buffer[m_pos])) RaiseError("malformatted line indicator");
+    unsigned n = 0; int digit = 0;
+    while ((digit = ConvertDigit(m_buffer[m_pos++], 10)) != -1) n = 10 * n + digit;
+    while (m_buffer[m_pos] == ' ' || m_buffer[m_pos] == '\t') m_pos++;
+    if (m_buffer[m_pos++] != '"') RaiseError("malformatted line indicator");
+    while (m_buffer[m_pos] != '"') m_pos++;
+    return n - 1;
+}
+
+int AsmFile::FindLastLineNumber(std::string& filename)
+{
+    long pos = m_pos;
+    long linebreaks = 0;
+    while (pos >= 0 && m_buffer[pos] != '#') {
+        if (m_buffer[pos] == '\n') linebreaks++;
+        pos--;
+    }
+    // 关键修正：如果找不到行标记，不要报错，直接使用默认值。
+    if (pos < 0) { filename = "unknown"; return 0; }
+
+    pos++; while (m_buffer[pos] == ' ' || m_buffer[pos] == '\t') pos++;
+    unsigned n = 0; int digit = 0;
+    while ((digit = ConvertDigit(m_buffer[pos++], 10)) != -1) n = 10 * n + digit;
+    while (m_buffer[pos] == ' ' || m_buffer[pos] == '\t') pos++;
+    if (m_buffer[pos++] != '"') { filename = "unknown"; return n + linebreaks - 1; }
+    filename.clear();
+    while (m_buffer[pos] != '"') { filename += m_buffer[pos++]; }
+    return n + linebreaks - 1;
+}
+
+std::string AsmFile::ReadIdentifier()
+{
+    long start = m_pos;
+    if (!IsIdentifierStartingChar(m_buffer[m_pos])) return std::string();
+    m_pos++;
+    while (IsIdentifierChar(m_buffer[m_pos])) m_pos++;
+    return std::string(&m_buffer[start], m_pos - start);
+}
     m_buffer = other.m_buffer;
     m_doEnum = other.m_doEnum;
     m_pos = other.m_pos;
