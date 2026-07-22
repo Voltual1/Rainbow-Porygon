14d59e33fe6bb51b89e0d4d17f4646a7393d916d
Voltual1 <18206039173@163.com>
2026-07-22 18:55:24 +0800

    Fix SIGSEGV crash on Android startup caused by invalid 0xE000000 Flash mapping and fix save file size bounds truncation bug

--- include/gba/flash_internal.h
+++ include/gba/flash_internal.h
@@ -1,7 +1,12 @@
 #ifndef GUARD_GBA_FLASH_INTERNAL_H
 #define GUARD_GBA_FLASH_INTERNAL_H
 
+#ifdef PORTABLE
+extern u8 gFlashBaseBuffer[];
+#define FLASH_BASE gFlashBaseBuffer
+#else
 #define FLASH_BASE ((u8 *)0xE000000)
+#endif
 
 #define FLASH_WRITE(addr, data) ((*(vu8 *)(FLASH_BASE + (addr))) = (data))
 
