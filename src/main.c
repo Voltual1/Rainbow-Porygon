ed9f0832186ae554a0bf945f1fea8570c20876cb
Voltual1 <18206039173@163.com>
2026-07-23 14:09:53 +0800

    Debug: Change startup backdrop color to Red to verify rendering pipeline on Android
    Change backdrop color to red for debugging purposes.

--- src/main.c
+++ src/main.c
@@ -95,7 +95,7 @@ void EnableVCountIntrAtLine150(void);
 
 void AgbMain(void)
 {
-    *(vu16 *)BG_PLTT = RGB_WHITE; // Set the backdrop to white on startup
+    *(vu16 *)BG_PLTT = RGB(31, 0, 0);// for debug
     InitGpuRegManager();
     REG_WAITCNT = WAITCNT_PREFETCH_ENABLE
  | WAITCNT_WS0_S_1 | WAITCNT_WS0_N_3
