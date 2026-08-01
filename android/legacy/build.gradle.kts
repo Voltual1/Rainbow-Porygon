ca5440f95de8c94603dae92000cc14c2d861e5e5
Voltual1 <18206039173@163.com>
2026-08-02 03:03:32 +0800

    Disable core library desugaring in build.gradle.kts

--- android/legacy/build.gradle.kts
+++ android/legacy/build.gradle.kts
@@ -60,7 +60,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-        isCoreLibraryDesugaringEnabled = true
+        isCoreLibraryDesugaringEnabled = false
     }
 
     buildTypes {
